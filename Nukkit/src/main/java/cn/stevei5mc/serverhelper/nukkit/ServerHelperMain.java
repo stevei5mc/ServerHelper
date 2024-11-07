6b7e7ff0cb1b974785fcd8c6fa64b2ea14dffaf6
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2024-11-07 14:18:32 +0800

    update

--- Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/ServerHelperMain.java
+++ Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/ServerHelperMain.java
@@ -1,6 +1,7 @@
 package cn.stevei5mc.serverhelper.nukkit;
 
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.Server;
 
 public class ServerHelperMain extends PluginBase {
     
@@ -11,6 +12,10 @@ public class ServerHelperMain extends PluginBase {
     }
     public void onEnable() {
         instance = this;
+        Server.getInstance().getScheduler().scheduleDelayedTask(this, () -> {
+            this.getLogger().warning("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
+            this.getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/ServerHelper");
+        },20);
     }
 
     public void onDisable() {
--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/ServerHelperMain.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/ServerHelperMain.java
@@ -11,7 +11,8 @@ public class ServerHelperMain extends Plugin {
     }
     
     public void onEnable() {
-        instance = this;
+        this.getLogger().warning("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
+        this.getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/ServerHelper");
     }
 
     public void onDisable() {
