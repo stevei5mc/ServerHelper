53087c30c6af411fe3e8c50486ad8ce3bda21c78
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2024-11-07 20:01:47 +0800

    fix WaterdogPE build

--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/ServerHelperMain.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/ServerHelperMain.java
@@ -11,7 +11,7 @@ public class ServerHelperMain extends Plugin {
     }
     
     public void onEnable() {
-        this.getLogger().warning("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
+        this.getLogger().warn("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
         this.getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/ServerHelper");
     }
 
