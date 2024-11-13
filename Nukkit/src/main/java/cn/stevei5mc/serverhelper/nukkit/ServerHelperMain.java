57d469cb4b79592ec7faafbdcb020115b63305a1
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2024-11-13 13:19:49 +0800

    update

--- Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/ServerHelperMain.java
+++ Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/ServerHelperMain.java
@@ -2,14 +2,37 @@ package cn.stevei5mc.serverhelper.nukkit;
 
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.Server;
+import cn.nukkit.utils.Config;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ServerHelperMain extends PluginBase {
     
     private static ServerHelperMain instance;
-    
+    private List<String> languages = Arrays.asList(
+            "bg_BG", "cs_CZ","da_DK","de_DE","el_GR","en_GB","en_US","es_ES","es_MX","fi_FI","fr_CA","fr_FR","hu_HU","id_ID","it_IT",
+            "ja_JP","ko_KR","nb_NO","nl_NL","pl_PL","pt_BR","pt_PT","ru_RU","sk_SK","sv_SE","tr_TR","uk_UA","zh_CN","zh_TW"
+    );
+    private List<String> settings = Arrays.asList("ban","kick","warn");
+    private Config config;
+
     public static ServerHelperMain getInstance() {
         return instance;
     }
+
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+        for (String language : languages) {
+            saveResource("language/base/"+language+".yml");
+            saveResource("language/custom/"+language+".yml");
+        }
+        for (String setting : settings) {
+            saveResource("Setting/"+setting+"yml");
+        }
+    }
+
     public void onEnable() {
         instance = this;
         Server.getInstance().getScheduler().scheduleDelayedTask(this, () -> {
@@ -19,6 +42,10 @@ public class ServerHelperMain extends PluginBase {
     }
 
     public void onDisable() {
-        this.getLogger().info("停止运行");
+        this.getLogger().info("已停止运行，感谢你的使用");
+    }
+
+    public Config getConfig() {
+        return config;
     }
 }
--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/ServerHelperMain.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/ServerHelperMain.java
@@ -2,24 +2,36 @@ package cn.stevei5mc.serverhelper.waterdogpe;
 
 import dev.waterdog.waterdogpe.plugin.Plugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ServerHelperMain extends Plugin {
     
     private static ServerHelperMain instance;
+    private List<String> languages = Arrays.asList(
+            "bg_BG", "cs_CZ","da_DK","de_DE","el_GR","en_GB","en_US","es_ES","es_MX","fi_FI","fr_CA","fr_FR","hu_HU","id_ID","it_IT",
+            "ja_JP","ko_KR","nb_NO","nl_NL","pl_PL","pt_BR","pt_PT","ru_RU","sk_SK","sv_SE","tr_TR","uk_UA","zh_CN","zh_TW"
+    );
+    private List<String> settings = Arrays.asList("ban","kick","warn");
     
     public static ServerHelperMain getInstance() {
         return instance;
     }
     
     public void onEnable() {
+        saveResource("config.yml");
+        for (String language : languages) {
+            saveResource("language/base/"+language+".yml");
+            saveResource("language/custom/"+language+".yml");
+        }
+        for (String setting : settings) {
+            saveResource("Setting/"+setting+".yml");
+        }
         this.getLogger().warn("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
         this.getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/ServerHelper");
     }
 
     public void onDisable() {
         this.getLogger().info("停止运行");
-    }
-
-    public void loadConfig() {
-
     }
 }
