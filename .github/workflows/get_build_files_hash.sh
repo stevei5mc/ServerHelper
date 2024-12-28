ee0d336bce892bc3a2af4f6aa44d157ba3256537
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2024-12-28 18:32:29 +0800

    fix script again

--- .github/workflows/get_build_files_hash.sh
+++ .github/workflows/get_build_files_hash.sh
@@ -4,6 +4,6 @@ ls -la
 echo "directory: $artifactName"
 sha256sum *.jar | tee "$artifactName"_edition-hash-sha256.txt
 md5sum *.jar | tee "$artifactName"_edition-hash-md5.txt
-mv ./"$artifactName"_editio-hash-sha256.txt ./"$artifactName"_edition-hash-sha256-$(sha256sum "$artifactName"_edition-hash-sha256.txt|awk '{print $1}').txt
+mv ./"$artifactName"_edition-hash-sha256.txt ./"$artifactName"_edition-hash-sha256-$(sha256sum "$artifactName"_edition-hash-sha256.txt|awk '{print $1}').txt
 mv ./"$artifactName"_edition-hash-md5.txt ./"$artifactName"_edition-hash-md5-$(md5sum "$artifactName"_edition-hash-md5.txt|awk '{print $1}').txt
 ls -la
