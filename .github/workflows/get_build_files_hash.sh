6f1e76803395717c081baab7d4894332abb850c8
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2024-12-28 17:43:45 +0800

    fix script

--- .github/workflows/get_build_files_hash.sh
+++ .github/workflows/get_build_files_hash.sh
@@ -1,7 +1,7 @@
 artifactName="$1"
 pwd
-sha256sum *.jar | tee $artifactName_edition-hash-sha256.txt
-md5sum *.jar | tee $artifactName_edition-hash-md5.txt
-mv ./$artifactName_edition-hash-sha256.txt ./$artifactName_edition-hash-sha256-$(sha256sum $artifactName_edition-hash-sha256.txt|awk '{print $1}').txt
-mv ./$artifactName_edition-hash-md5.txt ./$artifactName_edition-hash-md5-$(md5sum $artifactName_edition-hash-md5.txt|awk '{print $1}').txt
+sha256sum *.jar | tee "$artifactName_edition"-hash-sha256.txt
+md5sum *.jar | tee "$artifactName_edition"-hash-md5.txt
+mv ./"$artifactName_edition"-hash-sha256.txt ./"$artifactName_edition"-hash-sha256-$(sha256sum "$artifactName_edition"-hash-sha256.txt|awk '{print $1}').txt
+mv ./"$artifactName_edition"-hash-md5.txt ./"$artifactName_edition"-hash-md5-$(md5sum "$artifactName_edition"-hash-md5.txt|awk '{print $1}').txt
 ls -la
--- .github/workflows/organize_resources.sh
+++ .github/workflows/organize_resources.sh
@@ -1,9 +1,9 @@
 projectName="$1"
 cd staging
-cat *-edition-hash-sha256.txt | tee $projectName-all-edition-hash-sha256.txt
-cat *-edition-hash-md5.txt | tee $projectName-all-edition-hash-md5.txt
-mv ./$projectName-all-edition-hash-sha256.txt ./$projectName-all-edition-hash-sha256-$(sha256sum $projectName-all-edition-hash-sha256.txt|awk '{print $1}').txt
-mv ./$projectName-all-edition-hash-md5.txt ./$projectName-all-edition-hash-md5-$(md5sum $projectName-all-edition-hash-md5.txt |awk '{print $1}').txt
+cat *-edition-hash-sha256.txt | tee "$projectName"-all-edition-hash-sha256.txt
+cat *-edition-hash-md5.txt | tee "$projectName"-all-edition-hash-md5.txt
+mv ./"$projectName"-all-edition-hash-sha256.txt ./"$projectName"-all-edition-hash-sha256-$(sha256sum "$projectName"-all-edition-hash-sha256.txt|awk '{print $1}').txt
+mv ./"$projectName"-all-edition-hash-md5.txt ./"$projectName"-all-edition-hash-md5-$(md5sum "$projectName"-all-edition-hash-md5.txt |awk '{print $1}').txt
 ls -la
 cd ../
 mkdir -v github_releases
