36b55ad73a9e798b2bca4e5ee50f164a8a413587
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2025-05-07 11:39:36 +0800

    update workflows

--- .github/workflows/scripts/buildMavenProject.sh
+++ .github/workflows/scripts/buildMavenProject.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 artifactName="$1"
 projectPath="$2"
 cd ./$projectPath
--- .github/workflows/scripts/prepareReleasesFiles.sh
+++ .github/workflows/scripts/prepareReleasesFiles.sh
@@ -1,13 +1,14 @@
-projectName="$1"
+#!/bin/bash
 pwd
 ls -lah ./staging
 mkdir -vp bbs
+echo "--------------------"
 for edition in Nukkit WaterdogPE
 do
     mkdir -vp ./bbs/$edition
-    cp ./bbs/*-$edition*.* ./bbs/$edition
-    rm -rf ./bbs/$edition*-javadoc.jar
+    cp ./bbs/*$edition*.* ./bbs/$edition
+    rm -rf ./bbs/*$edition*-javadoc.jar   # javadoc 不会在论坛上发布，只在 github releases 上发布
     ls -lah ./bbs/$edition
     echo "--------------------"
 done
-ls -lah
+ls -la ./bbs
