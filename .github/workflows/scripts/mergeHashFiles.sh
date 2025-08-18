5b69a7e0069c22edfff99600491eec7d114ad339
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2025-08-18 17:08:13 +0800

    Update action

--- .github/workflows/scripts/mergeHashFiles.sh
+++ .github/workflows/scripts/mergeHashFiles.sh
@@ -6,11 +6,7 @@ oldDir="$(pwd)"
 cd $targetPath
 pwd
 echo "-------------------"
-cat *-edition.sha256.txt | tee "$hashFileSaveName".sha256.txt
-#mv ./"$projectName"-all-edition-hash-sha256.txt ./"$projectName"-all-edition-hash-sha256-$(sha256sum "$projectName"-all-edition-hash-sha256.txt|awk '{print $1}').txt
-#echo "-------------------"
-#cat *-edition.md5 | tee "$hashFileSaveName"-all-edition.md5
-#mv ./"$projectName"-all-edition-hash-md5.txt ./"$projectName"-all-edition-hash-md5-$(md5sum "$projectName"-all-edition-hash-md5.txt |awk '{print $1}').txt
+cat *-edition.sha256| tee "$hashFileSaveName".sha256
 echo "-------------------"
 ls -lah
 cd $oldDir
