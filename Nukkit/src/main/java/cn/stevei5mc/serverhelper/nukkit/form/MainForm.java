e19ee4f04c9f599c27d99a23d0a2444cef7c28a7
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2025-09-29 20:07:03 +0800

    update

--- Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/form/MainForm.java
+++ Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/form/MainForm.java
@@ -2,16 +2,19 @@ package cn.stevei5mc.serverhelper.nukkit.form;
 
 import cn.lanink.gamecore.form.element.ResponseElementButton;
 import cn.lanink.gamecore.form.windows.AdvancedFormWindowSimple;
+import cn.lanink.gamecore.utils.Language;
 import cn.nukkit.Player;
 import cn.stevei5mc.serverhelper.nukkit.form.manage.players.ManagePlayersForm;
 import cn.stevei5mc.serverhelper.nukkit.form.manage.server.ManageServerForm;
+import cn.stevei5mc.serverhelper.nukkit.utils.PluginI18n;
 import org.jetbrains.annotations.NotNull;
 
 public class MainForm {
     public static void mainMenu(@NotNull Player player) {
-        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple("ServerHelper","ServerHelper");
-        simple.addButton(new ResponseElementButton("管理玩家").onClicked(ManagePlayersForm::sendManagePlayersSystemUi));
-        simple.addButton(new ResponseElementButton("管理服务器").onClicked(ManageServerForm::sendManageServerUi));
+        Language baseLang = PluginI18n.getBaseLang(player);
+        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(baseLang.translateString("form-main-title"));
+        simple.addButton(new ResponseElementButton(baseLang.translateString("form-main-button-managerPlayer")).onClicked(ManagePlayersForm::sendManagePlayersSystemUi));
+        simple.addButton(new ResponseElementButton(baseLang.translateString("form-main-button-managerServer")).onClicked(ManageServerForm::sendManageServerUi));
         player.showFormWindow(simple);
     }
 }
--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/utils/LanguageApi.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/utils/LanguageApi.java
@@ -18,7 +18,7 @@ public class LanguageApi {
     }
 
     public String translateString(String key, Object... params) {
-        String value = this.language.getString(key, "§c" + key);
+        String value = this.language.getString(key, "§cUnknown text! key: " + key);
         if (params != null && params.length > 0) {
             for (int i = 1; i < params.length + 1; i++) {
                 value = value.replace("%" + i + "%", Objects.toString(params[i-1]));
@@ -29,28 +29,41 @@ public class LanguageApi {
 
     public void update(Configuration newLanguage) {
         boolean save = false;
-        HashMap<String, String> cache = new HashMap<>();
-        LinkedList<String> languageKeys = new LinkedList<>(language.getKeys());
-        for (String key : languageKeys) {
-            if (newLanguage.getKeys().contains(key)) {
-                cache.put(key, language.getString(key,"§cNo language text! key: "+key));
-            }else {
-                save = true;
+        int currentVersion = language.getInt("version", 0);
+        int latestVersion = newLanguage.getInt("version", 0);
+        boolean shouldUpdate = (currentVersion > latestVersion || currentVersion < latestVersion) && latestVersion > 0;
+        boolean noVersion = currentVersion == 0 && latestVersion == 0;
+        if (shouldUpdate || noVersion) {
+            HashMap<String, String> cache = new HashMap<>();
+            LinkedList<String> languageKeys = new LinkedList<>(language.getKeys());
+
+            if (!noVersion) {
+                language.setInt("version", latestVersion);
             }
-            language.remove(key);
-        }
-        for (String key : newLanguage.getKeys()) {
-            String value;
-            if(languageKeys.contains(key)) {
-                value = cache.get(key);
-            }else {
-                value = newLanguage.getString(key, "§cNo language text! key: "+key);
-                save = true;
+
+            for (String key : languageKeys) {
+                if (newLanguage.getKeys().contains(key)) {
+                    cache.put(key, language.getString(key, "§cNo language text! key: " + key));
+                } else {
+                    save = true;
+                }
+                language.remove(key);
             }
-            this.language.set(key, newLanguage.getString(key, value));
-        }
-        if (save) {
-            language.save();
+
+            for (String key : newLanguage.getKeys()) {
+                String value;
+                if (languageKeys.contains(key)) {
+                    value = cache.get(key);
+                } else {
+                    value = newLanguage.getString(key, "§cNo language text! key: " + key);
+                    save = true;
+                }
+                language.set(key, newLanguage.getString(key, value));
+            }
+
+            if (save) {
+                language.save();
+            }
         }
     }
 }
