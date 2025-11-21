e23ab513ae15d5ffa69afc048a25bf9818d46843
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2025-11-21 14:27:12 +0800

    update

--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/handler/JoinHandler.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/handler/JoinHandler.java
@@ -1,12 +1,18 @@
 package cn.stevei5mc.serverhelper.waterdogpe.handler;
 
+import cn.stevei5mc.serverhelper.waterdogpe.ServerHelperMain;
 import dev.waterdog.waterdogpe.network.connection.handler.IJoinHandler;
 import dev.waterdog.waterdogpe.network.serverinfo.ServerInfo;
 import dev.waterdog.waterdogpe.player.ProxiedPlayer;
 
+import java.util.List;
+import java.util.Random;
+
 public class JoinHandler implements IJoinHandler {
     @Override
     public ServerInfo determineServer(ProxiedPlayer proxiedPlayer) {
-        return null;
+        ServerHelperMain main = ServerHelperMain.getInstance();
+        List<String> servers = main.getProxy().getConfiguration().getPriorities();
+        return main.getProxy().getServerInfo(servers.get(new Random().nextInt(servers.size())));
     }
 }
