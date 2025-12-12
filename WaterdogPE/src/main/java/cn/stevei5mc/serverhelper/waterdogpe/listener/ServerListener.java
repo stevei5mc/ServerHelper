fbf3169ac1e013ee4ffb28774980b647e071ca09
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2025-12-12 17:40:17 +0800

    update

--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/listener/ServerListener.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/listener/ServerListener.java
@@ -1,13 +1,16 @@
 package cn.stevei5mc.serverhelper.waterdogpe.listener;
 
+import cn.stevei5mc.serverhelper.waterdogpe.ServerHelperMain;
 import dev.waterdog.waterdogpe.event.defaults.ServerTransferEvent;
 
 public class ServerListener {
 
+    private static final ServerHelperMain main = ServerHelperMain.getInstance();
 
     public static void onServerTransfer(ServerTransferEvent event) {
         String sourceServer = event.getSourceServer().getServerName();
         String targetServer = event.getTargetServer().getServerName();
         event.getPlayer().sendMessage("§a" + sourceServer + "  ==>>  " + targetServer);
+        main.getLogger().info("§aPlayer: " + event.getPlayer().getName() + " Server: " + sourceServer + "  ==>>  " + targetServer);
     }
 }
