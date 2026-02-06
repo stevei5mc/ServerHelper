18436779c31891b9e28fba9b6995d2c24da5d026
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-02-06 23:35:50 +0800

    update

--- Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/listener/PlayerListener.java
+++ Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/listener/PlayerListener.java
@@ -21,16 +21,18 @@ public class PlayerListener implements Listener {
         String message = event.getMessage().trim().toLowerCase();
         ArrayList<String> banCommands = new ArrayList<>(main.getBanCommands().getStringList("ban-commands"));
         if (!banCommands.isEmpty() && main.getBanCommands().getBoolean("enable",false)) {
-            for (String cmd : banCommands) {
-                String[] cmd2 = cmd.split("&");
+            for (String banCmd : banCommands) {
+                String[] banCmdConfig = banCmd.split("&");
+                String cmdName = banCmdConfig[0].toLowerCase();
+                String cmdPrefix = cmdName.startsWith("/") ? cmdName : "/" + cmdName;
                 String permission = BaseInfo.unbanCommandPermission;
-                if (message.startsWith(cmd2[0].trim().toLowerCase())) {
-                    if (cmd2.length >= 2 && !cmd2[1].isEmpty()) {
-                        permission = cmd2[1];
+                if (message.startsWith(cmdPrefix)) {
+                    if (banCmdConfig.length >= 2 && !banCmdConfig[1].isEmpty()) {
+                        permission = banCmdConfig[1];
                     }
-                    boolean isBanCmdWorld = cmd2.length < 3; // 在没有写禁用命令的世界时 = true
-                    if (cmd2.length == 3) {
-                        LinkedList<String> worldBanCmdList = new LinkedList<>(Arrays.asList(cmd2[2].split("%")));
+                    boolean isBanCmdWorld = banCmdConfig.length < 3; // 在没有写禁用命令的世界时 = true
+                    if (banCmdConfig.length == 3) {
+                        LinkedList<String> worldBanCmdList = new LinkedList<>(Arrays.asList(banCmdConfig[2].split("%")));
                         isBanCmdWorld = worldBanCmdList.contains(player.getLevel().getFolderName());
                     }
                     if (!player.hasPermission(permission) && isBanCmdWorld) {
@@ -42,9 +44,9 @@ public class PlayerListener implements Listener {
             }
         }
         if (main.getConfig().getBoolean("commands.usageLog.enable", true) && !event.isCancelled()) {
-            ArrayList<String> secretsCmd = new ArrayList<>(main.getConfig().getStringList("commands.usageLog.secretsList"));
-            if (!secretsCmd.isEmpty()) {
-                for (String secrets : secretsCmd) {
+            ArrayList<String> secretsCommands = new ArrayList<>(main.getConfig().getStringList("commands.usageLog.secretsList"));
+            if (!secretsCommands.isEmpty()) {
+                for (String secrets : secretsCommands) {
                     String cmd = secrets.startsWith("/") ? secrets : "/" + secrets;
                     if (message.startsWith(secrets)) {
                         message = secrets + " ***";
