3ac3d494174da3f535262718932d2726099382b8
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-02-08 20:12:09 +0800

    更新 wdpe 版本的 wdpe 核心的依赖版本

--- WaterdogPE/build.gradle.kts
+++ WaterdogPE/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     api(project(":ServerHelper-Common"))
-    compileOnly("dev.waterdog.waterdogpe:waterdog:2.0.3")
+    compileOnly("dev.waterdog.waterdogpe:waterdog:2.0.4-SNAPSHOT")
 }
 
 tasks{
