d1ba35ea1a521252a0a17ea13fd15ace2d2ff68a
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-02-14 12:11:21 +0800

    ci: fix

--- .github/workflows/scripts/getHashAndCopy.sh
+++ .github/workflows/scripts/getHashAndCopy.sh
@@ -4,7 +4,8 @@ oldDir="$(pwd)"
 version=$(grep "project.version=" "./gradle.properties" | sed 's/project.version=//')
 rm -rf ./Common/target/ServerHelper-Common-$version.jar   # 删除掉无用文件，因为这个文件是存放着通用的代码的（已在编译时一起打包进其他的jar里）
 for edition in Common Nukkit WaterdogPE
-    sha256sum "ServerHelper-$edition-*.jar" | tee "ServerHelper-$edition-$version.sha256"
+do
+    sha256sum "ServerHelper-$edition"-*.jar >> "ServerHelper-$edition-$version.sha256"
     ls -lah ./$edition/target/
     cp ./$edition/target/*.* ./staging
 done
