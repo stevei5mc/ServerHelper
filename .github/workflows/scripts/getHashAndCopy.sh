4465d57373f37b468cb1d0be5c87e4f4d2b50bf2
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-02-19 19:03:44 +0800

    update

--- .github/workflows/scripts/getHashAndCopy.sh
+++ .github/workflows/scripts/getHashAndCopy.sh
@@ -4,13 +4,15 @@ oldDir="$(pwd)"
 version=$(grep "project.version=" "./gradle.properties" | sed 's/project.version=//')
 rm -rf ./Common/target/ServerHelper-Common-$version.jar   # 删除掉无用文件，因为这个文件是存放着通用的代码的（已在编译时一起打包进其他的jar里）
 mkdir -vp staging
+echo "---------"
 for edition in Common Nukkit WaterdogPE
 do
-    cd ./$edition/target/
+    cd ./$edition/target/ || exit 1
     sha256sum "ServerHelper-$edition"-*.jar >> "ServerHelper-$edition-$version.sha256"
     ls -lah
     cd $oldDir
     cp ./$edition/target/*.* ./staging
+    echo "---------"
 done
 cd ./staging
 cat ServerHelper-*-$version.sha256 >> ServerHelper-$version-all_files.sha256 # 合并哈希记录到一个文件上
@@ -18,10 +20,10 @@ ls -lah
 # 在上传之前先校验文件一遍以防出在复制时出现问题
 sha256sum -c "ServerHelper-$version-all_files.sha256"
 if [ $? -ne 0 ]; then
-    echo "$hashSha256File 校验失败"
+    echo "校验失败"
     exit 1
 fi
-echo "$hashSha256File 校验成功。"
+echo "校验成功。"
 rm -f ./ServerHelper-*-$version.sha256 # 删除临时的哈希记录文件
 ls -lah
 cd ../
--- Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/listener/PlayerListener.java
+++ Nukkit/src/main/java/cn/stevei5mc/serverhelper/nukkit/listener/PlayerListener.java
@@ -48,7 +48,7 @@ public class PlayerListener implements Listener {
             if (!secretsCommands.isEmpty()) {
                 for (String secrets : secretsCommands) {
                     String cmd = secrets.startsWith("/") ? secrets : "/" + secrets;
-                    if (message.startsWith(secrets)) {
+                    if (message.startsWith(cmd)) {
                         message = secrets + " ***";
                     }
                 }
