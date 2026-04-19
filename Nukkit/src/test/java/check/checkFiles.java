ef98b0db615f6646b6dac4bbd3a575cebfa5f5a2
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-04-19 19:57:03 +0800

    优化 Nukiit 平台 plugin.yml 的权限节点检查

--- Nukkit/src/test/java/check/checkFiles.java
+++ Nukkit/src/test/java/check/checkFiles.java
@@ -1,10 +1,12 @@
 package check;
 
 import cn.nukkit.utils.Config;
+import cn.nukkit.utils.ConfigSection;
 import cn.stevei5mc.serverhelper.common.baseinfo.PermissionsInfo;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,10 +16,13 @@ public class checkFiles {
     public void checkPluginDotYmlFile() {
         Config config = new Config(Config.YAML);
         config.load(this.getClass().getResourceAsStream("/plugin.yml"));
-        ArrayList<String> permissions = new ArrayList<>(config.getSections("permissions").getAllMap().keySet());
-        System.out.println("已在 plugin.yml 配置的权限节点=" + permissions);
+        ArrayList<String> permissionsList = new ArrayList<>(config.getSections("permissions").getAllMap().keySet());
+        ConfigSection permissionsSection = config.getSections("permissions").getAll();
         for (PermissionsInfo permissionInfo: PermissionsInfo.values()) {
-            assertTrue(permissions.contains(permissionInfo.getPermission()), "无法寻找到指定权限节点=[" + permissionInfo.getPermission() + "]");
+            Map<String, Object> permissionMap = permissionsSection.getSection(permissionInfo.getPermission()).getAllMap();
+            assertTrue(permissionsList.contains(permissionInfo.getPermission()), "无法寻找到指定权限节点=[" + permissionInfo.getPermission() + "]");
+            assertEquals("op", String.valueOf(permissionMap.get("default")), "权限节点的归属权出现错误");
+            System.out.printf("权限节点=[%s]，归属=[%s]%n", permissionInfo.getPermission(), permissionMap.get("default"));
         }
     }
 }
