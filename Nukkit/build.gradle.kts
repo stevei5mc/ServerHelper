7a648e9c136fbe8be4424986ff0597b7294782f0
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-06-18 19:05:50 +0800

    update

--- Nukkit/build.gradle.kts
+++ Nukkit/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
     compileOnly(libs.nukkit)
     compileOnly("com.github.stevei5mc:AutoRestart:1.0.1")
     compileOnly("cn.lanink:MemoriesOfTime-GameCore:1.6.13")
-    compileOnly("com.smallaswater.ServerInfo:ServerInfo:1.1.0-SNAPSHOT")
     testImplementation(libs.nukkit)
 }
 
