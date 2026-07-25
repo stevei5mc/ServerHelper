5d396a73c3798fc09388782e94c4f83be2e19a92
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2026-07-25 20:07:46 +0800

    update

--- WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/commands/LobbyCmd.java
+++ WaterdogPE/src/main/java/cn/stevei5mc/serverhelper/waterdogpe/commands/LobbyCmd.java
@@ -3,6 +3,8 @@ package cn.stevei5mc.serverhelper.waterdogpe.commands;
 import cn.stevei5mc.serverhelper.waterdogpe.commands.base.CommandBase;
 import cn.stevei5mc.serverhelper.waterdogpe.serverinfo.LobbyServersInfo;
 import cn.stevei5mc.serverhelper.waterdogpe.serverinfo.WServerInfo;
+import cn.stevei5mc.serverhelper.waterdogpe.utils.LanguageApi;
+import cn.stevei5mc.serverhelper.waterdogpe.utils.PluginI18n;
 import dev.waterdog.waterdogpe.command.CommandSender;
 import dev.waterdog.waterdogpe.network.serverinfo.ServerInfo;
 import dev.waterdog.waterdogpe.player.ProxiedPlayer;
@@ -18,6 +20,7 @@ public class LobbyCmd extends CommandBase {
 
     @Override
     public boolean onExecute(CommandSender sender, String s, String[] args) {
+        LanguageApi lang = PluginI18n.getBaseLang(sender);
         if (sender.isPlayer()) {
             ProxiedPlayer player = (ProxiedPlayer) sender;
             if (args.length >= 1) {
@@ -25,37 +28,37 @@ public class LobbyCmd extends CommandBase {
                     int targetLobby = Integer.parseInt(args[0]);
                     int lobbyCount = LobbyServersInfo.getLobbyServerList().size();
                     if (targetLobby < 0 || targetLobby > lobbyCount) {
-                        player.sendMessage("§c大厅不存在！");
+                        player.sendMessage(lang.translateString("serverTransfer-lobbyServer-message-notFound"));
                         return true;
                     }
                     WServerInfo wServerInfo = LobbyServersInfo.getLobbyServerList().get(targetLobby - 1);
                     if (wServerInfo.isOnline()) {
                         if (wServerInfo.isFull()) {
-                            player.sendMessage("§c目标大厅已满员，请选择其他大厅");
+                            player.sendMessage(lang.translateString("serverTransfer-lobbyServer-message-isFull"));
                             return true;
                         }
                         player.connect(wServerInfo.getServerInfo());
                         return true;
                     }
-                    player.sendMessage("§c目标大厅处于离线状态，请选择其他大厅");
+                    player.sendMessage(lang.translateString("serverTransfer-lobbyServer-message-isOffline"));
                 }catch (NumberFormatException ignore) {
-                    player.sendMessage("§c请输入数字!");
+                    player.sendMessage(lang.translateString("serverTransfer-lobbyServer-message-invalidInput"));
                 }
             }else {
                 if (LobbyServersInfo.getLobbyServerList().isEmpty()) {
-                    sender.sendMessage("§c暂无可用的大厅服务器，请稍后再试！");
+                    sender.sendMessage(lang.translateString("serverTransfer-lobbyServer-message-noAvailable"));
                     return true;
                 }
                 ServerInfo serverInfo = LobbyServersInfo.findServer(player, player.getServerInfo());
                 if (serverInfo == null) {
-                    sender.sendMessage("§c暂无可用的大厅服务器，请稍后再试！");
+                    sender.sendMessage(lang.translateString("serverTransfer-lobbyServer-message-noAvailable"));
                     return true;
                 }
                 player.connect(serverInfo);
             }
             return true;
         }
-        sender.sendMessage("§c该命令只能由玩家执行！");
+        sender.sendMessage(lang.translateString(""));
         return true;
     }
 
@@ -71,6 +74,5 @@ public class LobbyCmd extends CommandBase {
     }
 
     @Override
-    public void sendHelp(CommandSender sender) {
-    }
+    public void sendHelp(CommandSender sender) {}
 }
